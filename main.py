47bdfd6621849b4ec2573871273928e3cbe9862f
Muhammad Fida Raditya <muhammad.120140037@student.itera.ac.id>
2023-09-05 15:51:59 +0700

    feat: siwtching to frame-to-frame detection

--- main.py
+++ main.py
@@ -4,10 +4,21 @@
 
 from ultralytics import YOLO
 import supervision as sv    
+import cv2
 
 def main():
+    # initiate model --> using pretrained yolov8
     model=YOLO("yolov8l.pt")
-    result=model.track(source=0,show=True)
+    # iterating each frame that tracked using "model.track" method
+    for result in model.track(source=0,show=False,stream=True):
+        frame=result.orig_img
+        
+        cv2.imshow("yolov8",frame)
+
+        # breaking the "for-loop"/closing the detection if Esc button triggered 
+        if(cv2.waitKey(30)==27):
+            break
+
 
 if __name__=="__main__":
     main()
