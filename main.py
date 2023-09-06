93d987b9167c723fa51d6ded200972c744a13163
Muhammad Fida Raditya <muhammad.120140037@student.itera.ac.id>
2023-09-06 13:10:50 +0700

    feat: add counter

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ from ultralytics import YOLO
 import supervision as sv    
 import cv2
 
+#create a line for counting
+#the default resolution for detection is 640x480
+res_height=int(480)
+# create a start and end of a line coordinate --> used for counting the object 
+START=sv.Point(0,res_height//2)
+END=sv.Point(640,res_height//2)
+
 def main():
     # initiate model --> using pretrained yolov8
     model=YOLO("yolov8l.pt")
+    
+    # create a line zone 
+    line_zone=sv.LineZone(start=START,end=END)
+    line_zone_annotator=sv.LineZoneAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=0.5
+    )
+    
     # create a bounding box using BoxAnnotator class from Supervision library
     box_annotator=sv.BoxAnnotator(
         thickness=2,
@@ -36,7 +52,16 @@ def main():
         ]
         
         # create box annotator using "frame" variable and detections that initiated in "detections" variables
-        frame=box_annotator.annotate(scene=frame,detections=detections,labels=labels)
+        frame=box_annotator.annotate(
+            scene=frame,
+            detections=detections,
+            labels=labels
+        )
+
+        # annotate the line zone
+        line_zone.trigger(detections=detections)
+        line_zone_annotator.annotate(frame=frame,line_counter=line_zone)
+
         # show the result of the annotated frame
         cv2.imshow("yolov8",frame)
         
