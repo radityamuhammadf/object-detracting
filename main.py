0971a247e01f6ad76d593216e2c51181031897f2
Muhammad Fida Raditya <muhammad.120140037@student.itera.ac.id>
2023-09-06 08:56:34 +0700

    feat: create tracking and filter the detection class

--- main.py
+++ main.py
@@ -16,17 +16,32 @@ def main():
         text_scale=0.5
     )
     # iterating each frame that tracked using "model.track" method
-    for result in model.track(source=0,show=False,stream=True):
+    for result in model.track(source=1,show=False,stream=True):
         # set frame variable using the original image (orig_img attribute) from method output
         frame=result.orig_img
         # call YOLOv8 for image detection
         detections=sv.Detections.from_yolov8(result)
+        # create an tracker instance 
+        if result.boxes.id is not None:
+            detections.tracker_id=result.boxes.id.cpu().numpy().astype(int)
+        
+        # filter only "person" class that will be detected
+        detections=detections[detections.class_id==0]
+
+        #instatiate labels variable
+        labels=[
+            f"#{tracker_id} - {model.model.names[class_id]} {confidence:0.2f}"
+            for _,confidence,class_id,tracker_id
+            in detections
+        ]
+        
         # create box annotator using "frame" variable and detections that initiated in "detections" variables
-        frame=box_annotator.annotate(scene=frame,detections=detections)
+        frame=box_annotator.annotate(scene=frame,detections=detections,labels=labels)
+        # show the result of the annotated frame
         cv2.imshow("yolov8",frame)
         
         # breaking the "for-loop"/closing the detection if Esc button triggered 
-        if(cv2.waitKey(30)==27):
+        if(cv2.waitKey(10)==27):
             break
 
 
